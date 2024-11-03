88f2eb44d717c5b27869f5508b9c534b99417aca
dloh2236 <94659651+dloh2236@users.noreply.github.com>
2024-11-03 16:31:06 +0800

    Bug fix frontend

--- frontend/peerprep/tailwind.config.js
+++ frontend/peerprep/tailwind.config.js
@@ -8,32 +8,15 @@ module.exports = {
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-  	extend: {
-  		fontFamily: {
-  			sans: ["var(--font-sans)"],
-  			mono: ["var(--font-mono)"],
-  			fun: ["var(--font-fun)"]
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {
-  			sidebar: {
-  				DEFAULT: 'hsl(var(--sidebar-background))',
-  				foreground: 'hsl(var(--sidebar-foreground))',
-  				primary: 'hsl(var(--sidebar-primary))',
-  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-  				accent: 'hsl(var(--sidebar-accent))',
-  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-  				border: 'hsl(var(--sidebar-border))',
-  				ring: 'hsl(var(--sidebar-ring))'
-  			}
-  		}
-  	}
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        mono: ["var(--font-mono)"],
+        fun: ["var(--font-fun)"],
+      },
+    },
   },
-  darkMode: ["class", 'class'],
   darkMode: "class",
- plugins: [nextui(), require("tailwindcss-animate")],
+  darkMode: "class",
+ plugins: [nextui()],
 }
